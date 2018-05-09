57e34439e7b19ad76e9323e989e73843338248df
dr-matt-smith <dr_matt_smith@me.com>
2018-05-09 15:55:10 +0100

    added files to project

---
+++ Assets/SimpleFSM/MyGameManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MyGameManager : MonoBehaviour 
+{
+    public Text textCurrentState;
+    public Text textStarsCollected;
+    public Text textSecondsLeft;
+
+    public float secondsLeft = 10;
+    public int totalStarsToBeCollected = 2;
+    private int starsColleted = 0;
+
+    // extra health feature
+    public Text textHealth;
+    private float health = 1;
+
+	void Update()
+	{
+        // extra feature
+        RandomlyChangeHealth();
+
+        secondsLeft -= Time.deltaTime;
+        UpdateDisplays();
+	}
+
+	public void DisplayCurrentState(State currentState)
+    {
+        textCurrentState.text = currentState.name;
+    }
+
+    public bool HasCollectedAllStars()
+    {
+        return (starsColleted == totalStarsToBeCollected);
+    }
+
+    public float GetTimeRemaining()
+    {
+        return secondsLeft;
+    }
+
+    public void BUTTON_ACTION_PickupOneStar()
+    {
+        starsColleted++;
+    }
+
+    private void UpdateDisplays()
+    {
+        textStarsCollected.text = "stars = " + starsColleted;
+        textSecondsLeft.text = "time left = " + secondsLeft;
+
+        // extra freature
+        textHealth.text = "health = " + health;
+    }
+
+    // extra freature
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    // health can't go below 0 or above 1
+    private void RandomlyChangeHealth()
+    {
+        float healthChange = Random.Range(-0.5f, 0.5f);
+        health += healthChange;
+        health = Mathf.Clamp(health, 0, 1);
+    }
+
+}
